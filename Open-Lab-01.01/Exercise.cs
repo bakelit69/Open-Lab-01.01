111e3bff9803237e280f946f084f2a3df02b7c05
bakelit69 <113536297+bakelit69@users.noreply.github.com>
2022-09-21 19:58:01 +0200

    Update Exercise.cs

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._01
 {
@@ -6,7 +6,15 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();
+            int num = int.Parse(input);
+
+                if (num < 10) { 
+                return true;
+                } 
+                else {
+                   return false;
+                }
+           throw new NotImplementedException();
         }
     }
 }
